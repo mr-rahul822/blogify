0bf1062fb86af895095d6a5ed7cfac937aa82292
Dark <dark.devil1055@gmail.com>
2025-07-03 13:16:49 +0530

    redis error solving

--- config/redis.js
+++ config/redis.js
@@ -24,7 +24,10 @@ const redisClient = redis.createClient({
     password: process.env.REDIS_DB_PASS,
     socket: {
         host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDIS_PORT, 10) || 6379// ✅ always parse to number
+        port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+        family: 4,
+        connectTimeout: 10000,
+        lazyConnect: true // ✅ always parse to number
     }
 });
 
