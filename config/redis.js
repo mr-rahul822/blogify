1152c0ba499ba1e0137f64d4082ab160a0c7fe04
Dark <dark.devil1055@gmail.com>
2025-07-03 16:45:02 +0530

    redis error solving

--- config/redis.js
+++ config/redis.js
@@ -44,7 +44,7 @@ const redis = require("redis");
 // Add debugging to see what values are being used
 console.log('Redis Config:', {
     host: process.env.REDIS_URL,
-    port: process.env.REDISPORT,
+    port: process.env.REDIS_PORT,
     user: process.env.REDIS_DB_USER,
     hasPassword: !!process.env.REDIS_DB_PASS
 });
@@ -54,7 +54,7 @@ const redisClient = redis.createClient({
     password: process.env.REDIS_DB_PASS,
     socket: {
         host: process.env.REDIS_URL,
-        port: parseInt(process.env.REDISPORT, 10) || 6379,
+        port: parseInt(process.env.REDIS_PORT, 10) || 6379,
         family: 4,
         connectTimeout: 10000,
         lazyConnect: true
@@ -68,5 +68,16 @@ redisClient.on('error', (err) => {
 redisClient.on('connect', () => {
     console.log('Connected to Redis Cloud successfully');
 });
+
+
+
+
+console.log('=== ENVIRONMENT VARIABLES DEBUG ===');
+console.log('REDIS_URL:', process.env.REDIS_URL);
+console.log('REDIS_PORT:', process.env.REDIS_PORT);
+console.log('REDIS_DB_USER:', process.env.REDIS_DB_USER);
+console.log('REDIS_DB_PASS:', process.env.REDIS_DB_PASS ? '***SET***' : 'NOT SET');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('=== END DEBUG ===');
 
 module.exports = redisClient;
